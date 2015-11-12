724ed1d6e3a4332f764c001db72c6bacfcb07337
Gabriel Matias <gabriel.2m.deleon@gmail.com>
2015-11-12 12:39:15 -0800

    working chat service and server

---
+++ client/app/messages/messages.js
@@ -0,0 +1,21 @@
+var myApp = angular.module("myApp", ["firebase"]);
+
+
+myApp.controller("MessagesController", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) {
+    var ref = new Firebase("https://gitlytest.firebaseio.com/messages");
+    $scope.messages = $firebaseArray(ref);
+
+    //ADD MESSAGE METHOD
+    $scope.addMessage = function(e) {
+      //LISTEN FOR RETURN KEY & IF MESSAGE IS NOT ""
+      if (e.keyCode === 13 && $scope.msg) {
+        //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+        var name = $scope.name || "anonymous";
+        $scope.messages.$add({ user: name, userMessage: $scope.msg });
+        //RESET MESSAGE
+        $scope.msg = "";
+      }
+    };
+  }
+]);
