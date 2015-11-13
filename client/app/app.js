22f90813061e51584d4795ddc1b35ecc6a09e0cf
Gabriel Matias <gabriel.2m.deleon@gmail.com>
2015-11-12 17:22:35 -0800

    refactored the way our controller was set up and began inplementing uiRouter

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,19 @@
 //app module goes here!!!
+angular.module("myApp", [
+	"firebase",
+	"ui.router",
+	"myApp.messages"
+])
 
+.config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider.state("home", {
+		url: "/",
+		template: "<h1>Hello</h1>"
+	});
+});
 
 //route manager (.config) also goes here
 
